8ebee9e26cd8315705152c42ad1a2209ec64d981
Radek Jancik <radek.jancik@usys.cz>
2025-10-21 20:43:01 +0200

    feat: Replace variables names with self-describe names

--- SunamoThreading/Downloading/InputDownload.cs
+++ SunamoThreading/Downloading/InputDownload.cs
@@ -1,3 +1,5 @@
+// EN: Variable names have been checked and replaced with self-descriptive names
+// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
 namespace SunamoThreading.Downloading;
 /// <summary>
 /// Obsahuje URI ke stažení - každá URI má jedinečné identifikační číslo
